566423d7bf6627b2c995b2383d6142616bf81f7e
Deepank Vora <deepankvora@gmail.com>
2024-11-21 12:49:12 +0530

    Initial commit

---
+++ app/src/main/java/com/bytesalt/runpad_codemagic_example_android/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.bytesalt.runpad_codemagic_example_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bytesalt.runpad_codemagic_example_android.ui.theme.RunpadcodemagicexampleandroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RunpadcodemagicexampleandroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RunpadcodemagicexampleandroidTheme {
+        Greeting("Android")
+    }
+}
